922afe101697a77391dd4be9e6d2bd27f6a86293
Daniel Billotte <daniel.billotte@galvanize.com>
2025-03-31 13:45:53 -0700

    changed arror functions into function functions

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Generate a random number between 1 and n
-const randomInt = (n) => Math.floor(Math.random() * n) + 1;
+function randomInt(n) {
+    return Math.floor(Math.random() * n) + 1;
+}
 
 
-const play = () => {
-
+function play() {
     let numberToGuess = randomInt(101); // Generate a random number between 1 and 100
     console.log(`Hello World!, let's make a guessing game! here's a number to guess ${numberToGuess}`);
 
